fff76e3b4d1efe39e0803b25639a057b935e6fda
lyhongkhanh <khanh.lyhong.it@gmail.com>
2025-07-02 22:16:35 +0700

    input package

--- target/generated-sources/annotations/org/project/social_account_business/mapper/PaymentTransactionMapperImpl.java
+++ target/generated-sources/annotations/org/project/social_account_business/mapper/PaymentTransactionMapperImpl.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-07-02T21:59:49+0700",
+    date = "2025-07-02T22:15:42+0700",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.9 (Oracle Corporation)"
 )
 @Component
